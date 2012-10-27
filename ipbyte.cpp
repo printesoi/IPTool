9f1e2f85512e9278e965d414e3709533a5a9556a
Victor Dodon <dodonvictor@gmail.com>
2012-10-27 06:01:14 +0300

    Added copyright information

--- ipbyte.cpp
+++ ipbyte.cpp
@@ -1,3 +1,23 @@
+/* ============================================================
+*
+* Date        : 2012-10-27
+* Description : A simple GUI program written in Qt to calculate network
+                adress and broadcast address given the IP and the mask
+*
+* Copyright (C) 2012 Dodon Victor <victordodon at gmail dot com>
+*
+* This program is free software; you can redistribute it
+* and/or modify it under the terms of the GNU General
+* Public License as published by the Free Software Foundation;
+* either version 2, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* ============================================================ */
+
 #include <QGridLayout>
 #include <QFocusEvent>
 #include <QDebug>
@@ -16,6 +36,10 @@ IPByte::IPByte(QWidget *parent) :
     m_lineEdit = new InputField(this);
     m_lineEdit->setAlignment(Qt::AlignHCenter);
     updateText();
+
+    QWidget* widget = parent ? parent : this;
+    int height = widget->fontMetrics().height() + 5;
+    m_lineEdit->setFixedHeight(height);
 
     m_layout->addWidget(m_lineEdit, 0, 0, 1, 8);
     for (int i = 0; i < 8; ++i)
--- ipv4.cpp
+++ ipv4.cpp
@@ -1,3 +1,23 @@
+/* ============================================================
+*
+* Date        : 2012-10-27
+* Description : A simple GUI program written in Qt to calculate network
+                adress and broadcast address given the IP and the mask
+*
+* Copyright (C) 2012 Dodon Victor <victordodon at gmail dot com>
+*
+* This program is free software; you can redistribute it
+* and/or modify it under the terms of the GNU General
+* Public License as published by the Free Software Foundation;
+* either version 2, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* ============================================================ */
+
 #include <QString>
 #include "ipv4.h"
 
